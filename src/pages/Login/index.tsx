c21ece310ac13167037243fc9005974f8f3ff441
JotaSouza <jotabccif@gmail.com>
2022-05-21 16:18:14 -0300

    feat: create routes

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,3 @@
+import { Heading } from "@chakra-ui/react";
+
+export const Login = () => <Heading as="h1">Login</Heading>;
---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+// import { Login } from "../pages/Login";
+
+export const RoutesApp = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    {/* <Route path="/login" element={<Login/>} */}
+  </Routes>
+);
